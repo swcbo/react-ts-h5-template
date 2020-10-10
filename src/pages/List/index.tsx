5718bc08132add94fe2bcbe69ac7c7dba9cefffd
huangbo <1104133609@qq.com>
2020-10-10 22:41:17 +0800

    添加

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import Toast from '@/layout/Toast'
+import React, { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 const List: React.FC = () => {
     const history = useHistory()
+    useEffect(() => {
+        Toast.success()
+    }, [])
     return <div onClick={() => history.push('/other')} style={{ backgroundColor: 'green' }}>
     </div>
 }
--- src/plugins/request.ts
+++ src/plugins/request.ts
@@ -1,4 +1,4 @@
-import Toast from '@/components/Toast';
+import Toast from '@/layout/Toast';
 import axios, { AxiosRequestConfig } from 'axios';
 /*
  * @Descripttion:
@@ -6,7 +6,7 @@ import axios, { AxiosRequestConfig } from 'axios';
  * @Author: 小白
  * @Date: 2020-10-04 13:11:29
  * @LastEditors: 小白
- * @LastEditTime: 2020-10-10 21:09:22
+ * @LastEditTime: 2020-10-10 22:39:02
  */
 import { getAuth, setAuth } from './../utils/index';
 
