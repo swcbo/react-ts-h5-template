3647404eb92a645ad2a6b19bc9a73b854f2b889f
huangbo <1104133609@qq.com>
2020-12-21 17:02:00 +0800

    fix:修改publicpath

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,7 @@
  * @Author: 小白
  * @Date: 2020-06-21 15:28:19
  * @LastEditors: 小白
- * @LastEditTime: 2020-12-21 16:54:46
+ * @LastEditTime: 2020-12-21 16:59:35
  */
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 const packageinfo = require('./package.json');
@@ -70,7 +70,7 @@ const alter_config = () => (config) => {
 module.exports = override(
 	alter_config(),
 	useEslintRc('./package.json'),
-	setWebpackPublicPath(IS_PRODUCTION ? './' : './'),
+	// setWebpackPublicPath(IS_PRODUCTION ? './' : './'),
 	addWebpackExternals(
 		IS_PRODUCTION
 			? {
