851642019c08218c045082fefea4701d1bee3b8a
swcbo <swcbo@users.noreply.github.com>
2022-01-05 10:42:16 +0000

    deploy: 1e32482c3d3545a385cff8d376af3c4957ae1119

--- assets/index.4cffb217.js
+++
@@ -1,1 +0,0 @@
-import{r,R as e}from"./react.ca74714e.js";import{u as o}from"./index.f646073d.js";var t=r.exports.memo((()=>{const r=o();return e.createElement("div",{onClick:()=>{r(-1)},style:{backgroundColor:"red"}},"32132131")}));export{t as default};
