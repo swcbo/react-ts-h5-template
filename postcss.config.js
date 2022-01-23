3feaad637ca8a3a4352bc5f648b5c2f08b42c6be
huangbo <1104133609@qq.com>
2022-01-23 13:42:59 +0800

    feat:回复tailwind引入

--- postcss.config.js
+++ postcss.config.js
@@ -3,13 +3,13 @@ const tailwindcss = require('tailwindcss');
 const px2rem = require('postcss-pxtorem');
 module.exports = {
   plugins: [
-    // tailwindcss(),
+    tailwindcss(),
     autoprefixer(),
     px2rem({
       rootValue: 75,
       unitPrecision: 5,
       propList: ['*'],
-      exclude: /node_modules/,
+      // exclude: /node_modules/,
     }),
   ],
 };
