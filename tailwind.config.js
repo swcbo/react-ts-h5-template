7532608dff1f1188183e7c81c37feb04f7cbf36b
huangbo <1104133609@qq.com>
2022-01-23 14:31:57 +0800

    feat:去掉tailwindcss base默认样式

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  purgeCSS: {
-    whitelistPatterns: [/am-.+$/],
-    whitelistPatternsChildren: [/am-.+$/],
-  },
   theme: {
     extend: {},
   },
